458d8bab23c589555a9a300e43e0113cea45a5ff
Kamil Szpak <dev@kamilszpak.com>
2025-09-14 13:56:00 +0200

    Update localization files and prompt fallback

--- Sources/SearchBar/Views/SearchFieldView.swift
+++ Sources/SearchBar/Views/SearchFieldView.swift
@@ -40,7 +40,7 @@ struct SearchFieldView: View{
         Group{
             if #available(iOS 17.0, macOS 14.0, *) {
                 ZStack{
-                    TextField(prompt, text: $text)
+                    TextField(prompt ?? SearchBarTranslation.prompt.value, text: $text)
                         .textFieldStyle(.plain)
                         .focused($isEditing)
                         .foregroundStyle(style.textColor ?? .primary)
